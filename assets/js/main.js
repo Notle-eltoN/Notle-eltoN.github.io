94c751c66f84b1a76b05e6cf3803945f4ccb909e
Elton Chang <80650461+Notle-eltoN@users.noreply.github.com>
2024-09-10 21:23:03 -0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,95 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*
+	Future Imperfect by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
+(function($) {
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+	var	$window = $(window),
+		$body = $('body'),
+		$menu = $('#menu'),
+		$sidebar = $('#sidebar'),
+		$main = $('#main');
 
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
+	// Menu.
+		$menu
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
 
-/*==================== REMOVE MENU MOBILE ====================*/
+	// Search (header).
+		var $search = $('#search'),
+			$search_input = $search.find('input');
 
+		$body
+			.on('click', '[href="#search"]', function(event) {
 
-/*==================== ACCORDION SKILLS ====================*/
+				event.preventDefault();
 
+				// Not visible?
+					if (!$search.hasClass('visible')) {
 
-/*==================== QUALIFICATION TABS ====================*/
+						// Reset form.
+							$search[0].reset();
 
+						// Show.
+							$search.addClass('visible');
 
-/*==================== SERVICES MODAL ====================*/
+						// Focus input.
+							$search_input.focus();
 
+					}
 
-/*==================== PORTFOLIO SWIPER  ====================*/
+			});
 
+		$search_input
+			.on('keydown', function(event) {
 
-/*==================== TESTIMONIAL ====================*/
+				if (event.keyCode == 27)
+					$search_input.blur();
 
+			})
+			.on('blur', function() {
+				window.setTimeout(function() {
+					$search.removeClass('visible');
+				}, 100);
+			});
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+	// Intro.
+		var $intro = $('#intro');
 
+		// Move to main on <=large, back to sidebar on >large.
+			breakpoints.on('<=large', function() {
+				$intro.prependTo($main);
+			});
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+			breakpoints.on('>large', function() {
+				$intro.prependTo($sidebar);
+			});
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+})(jQuery);
